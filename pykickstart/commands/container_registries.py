5fe9ce0bcf225fcd63743474198d218858eafcbf
Eldar Yusupov <eyusupov@gmail.com>
2019-07-13 12:01:20 +0300

    Enhance container_registries command

--- pykickstart/commands/container_registries.py
+++ pykickstart/commands/container_registries.py
@@ -2,11 +2,13 @@ from pykickstart.version import F29
 from pykickstart.base import KickstartCommand
 from pykickstart.errors import KickstartParseError
 from pykickstart.options import KSOptionParser, commaSplit
+from pykickstart.i18n import _
 
 class F29_ContainerRegistries(KickstartCommand):
     def __init__(self, writePriority=11, *args, **kwargs):
         KickstartCommand.__init__(self, writePriority, *args, **kwargs)
-        self.urls = kwargs.get("urls", None)
+        self.search = kwargs.get("search", [])
+        self.insecure = kwargs.get("insecure", [])
         self.op = self._getParser()
 
     def _getParser(self):
@@ -14,23 +16,35 @@ class F29_ContainerRegistries(KickstartCommand):
                             Setup container registries that will be used to pull
                             boot container image and additional images, if any are specified.""",
                             version=F29)
-        op.add_argument("urls",
-                        metavar="[urls]",
-                        nargs='+',
+        op.add_argument("--search",
+                        type=commaSplit,
+                        metavar="<search1>,<search2>,...,<searchN>",
                         help="Registry URLs",
+                        version=F29)
+        op.add_argument("--insecure",
+                        type=commaSplit,
+                        metavar="<insecure1>,<insecure2>,...,<insecureN>",
+                        help="Insecure registries",
                         version=F29)
         return op
 
     def __str__(self):
         retval = KickstartCommand.__str__(self)
-        if not self.urls:
+        if not self.seen:
             return retval
 
-        retval += "container_registries " + ' '.join(self.urls) + "\n"
+        retval += "container_registries"
+        if self.search:
+            retval += " --search=" + ",".join(self.search)
+        if self.insecure:
+            retval += " --insecure=" + ",".join(self.insecure)
 
+        retval += "\n"
         return retval
 
     def parse(self, args):
+        if len(args) == 0:
+            raise KickstartParseError(_("options need to be specified for container_registries command"), lineno=self.lineno)
         ns = self.op.parse_args(args=args, lineno=self.lineno)
         self.set_to_self(ns)
         return self
--- tests/commands/container_registries.py
+++ tests/commands/container_registries.py
@@ -8,10 +8,16 @@ class F29_TestCase(CommandTest):
     def runTest(self):
         self.assert_parse_error("container_registries")
 
-        self.assert_parse("container_registries registry.fedoraproject.org",
-                          "container_registries registry.fedoraproject.org\n")
+        self.assert_parse("container_registries --search registry.fedoraproject.org",
+                          "container_registries --search=registry.fedoraproject.org\n")
 
+        self.assert_parse("container_registries --search registry.fedoraproject.org,docker.io",
+                          "container_registries --search=registry.fedoraproject.org,docker.io\n")
+
+        self.assert_parse("container_registries --search registry.fedoraproject.org,docker.io,localhost:5000 --insecure localhost:5000",
+                          "container_registries --search=registry.fedoraproject.org,docker.io,localhost:5000 --insecure=localhost:5000\n")
 
         command = self.handler().commands["container_registries"]
-        self.assertEqual(command.urls, None)
+        self.assertEqual(command.insecure, [])
+        self.assertEqual(command.search, [])
         self.assertEqual(command.__str__(), "")
